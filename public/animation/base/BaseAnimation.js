d042be22979caaa5c8e78b0577782284193f7fab
xmZhu <zhuxianming2@163.com>
2013-04-22 16:20:20 +0800

    动画基础类
    动画基础类

---
+++ public/animation/base/BaseAnimation.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: rongwang
+ * Date: 13-4-18
+ * Time: 上午11:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+/**
+ * 基础动画类<br/>
+ * 为了其他动画类提供基础方法，对dom操作的简单封装
+ * @class BaseAnimation
+ * @module animation 动画模块
+ * @namespace Desktop.animation
+ */
+Ext.define('Desktop.animation.BaseAnimation',{
+
+})
