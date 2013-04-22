591134bacf247a1ce43bd944342a3e451afb6de2
xmZhu <zhuxianming2@163.com>
2013-04-22 16:19:56 +0800

    api
    api文件结构提交

---
+++ api/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Desktop.animation.BaseAnimation",
+        "Desktop.animation.ShakeAnimation"
+    ],
+    "modules": [
+        "animation 动画模块"
+    ],
+    "allModules": [
+        {
+            "displayName": "animation 动画模块",
+            "name": "animation 动画模块",
+            "description": "基础动画类<br/>\n为了其他动画类提供基础方法，对dom操作的简单封装"
+        }
+    ]
+} };
+});
